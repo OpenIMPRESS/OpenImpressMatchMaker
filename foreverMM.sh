60edaf0ef3237adc447a742bcc5bd81fbf42ffd5
Jan Kolkmeier <jankolkmeier@gmail.com>
2017-10-31 13:01:52 +0100

    script now compiles and starts service

--- foreverMM.sh
+++ foreverMM.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 BASEDIR=$(dirname "$0")
-cd "$BASEDIR"/MMBackend &&
+cd "$BASEDIR"/MMFrontend &&
+ng build --environment=prod &&
+cd ../MMBackend &&
+tsc -p ./tsconfig.json &&
 forever stopall &&
 forever start ./startServer.js &&
 forever start ./startMM.js
